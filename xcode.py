a99b97051e5835200608c7e872a1fb71a9533122
audyXcode <76478626+schizophrenics4@users.noreply.github.com>
2021-01-01 07:03:51 +0700

    Add files via upload

---
+++ xcode.py
@@ -0,0 +1,80 @@
+import os
+import base64
+import time
+
+def xcode_banner():
+    BOLD='\033[1;37m'
+    time.sleep(1)
+    x=base64.b64decode("4paI")
+    a=base64.b64decode("4pWU")
+    l=base64.b64decode("4pWa")
+    j=base64.b64decode("4pWX")
+    i=base64.b64decode("4pWR")
+    n=base64.b64decode("4pWQ")
+    c=base64.b64decode("4pWd")
+    me="                  \033[1;41m       ..:: audy X zollam from Xcode ::..       \033[0m%s\n"%(BOLD)
+    PL="                  \033[34;47;2m               <++++ XNCODE ++++>               \033[0;49m"
+    R="\033[31m"
+    W="\033[37m"
+    os.system("clear")
+    print BOLD
+    print "%s                  {}%s    {}%s{}   {}%s    {}%s  {}%s  {}%s   {}%s{}%s".format(a,a,W,a,a,a,a,a,a)%(R,x*3,x*3,x*2,x*2,x*2,x*6,x*2,x*2)
+    print "%s                  {}{}%s  {}%s{}    {}{}{}    {}{}{}  {}{}{}  {}{}{}   {}{}{}{}{}{}".format(l,j,a,W,l,n,c,l,n,c,l,n,c,l,n*5,c,l,n,c,l,n,c)%(R,x*3,x*3)
+    print "%s                   {}{}%s{}%s{}    {}%s  {}%s  {}%s   {}%s".format(l,j,a,W,a,a,a,a)%(R,x*3,x*3,x*5,x*7,x*6,x*5)
+    print "%s                    {}{}%s{}     {}%s{}{}  {}%s{}{}%s  {}%s{}{}%s  {}%s{}{}".format(l,j,W,i,n*2,c,i,n*2,j,i,n*2,j,i,n*2,c)%(R,x*5,x*2,x*2,x*2,x*2,x*2,x*2)
+    print "%s                    {}%s{}%s{}    {}%s     {}%s  {}%s  {}%s  {}%s  {}%s".format(a,j,W,i,i,i,i,i,i)%(R,x*3,x*3,x*2,x*2,x*2,x*2,x*2,x*5)
+    print "%s                   {}%s {}{}%s{}   {}%s     {}%s  {}%s  {}%s  {}%s  {}%s{}{}".format(a,l,j,W,i,i,i,i,i,i,n*2,c)%(R,x*3,x*3,x*2,x*2,x*2,x*2,x*2,x*2)
+    print "%s                  {}%s   {}{}%s{}  {}%s  {}%s  {}%s   {}%s".format(a,l,j,W,i,i,i,i)%(R,x*3,x*3,x*5,x*7,x*6,x*5)
+    print "%s                  {}{}{}    {}{}{}{}  {}{}{}  {}{}{}  {}{}{}   {}{}{}".format(l,n*2,c,l,n*2,c,W,l,n*4,c,l,n*6,c,l,n*5,c,l,n*4,c)%(R)
+    print me+PL
+
+n='\033[35;41m-\033[2;0m'
+ef='\033[2m'
+fe='\033[0;1m'
+
+aint="""
+
+ \  //  ____ _    ____ ____ ___              __  __        ___
+  \// _ |__| |    |  | |__| |_    __  _   _  \ \/ /________| |___.
+  //\   |    |__| |__| | \_ |__   |_| |___|   >  <| _| _ | _ | +_|
+ //  \  HIDDEN YOUR PRIVACY NOW   |__|  |    /_/\_\__|___|___|___|
+
+%s ..oi000MMMMMMMMMMM00oo++..
+                ''""+0MMM0o.
+                      +OOOOo.
+            ...+o0o+..   +00o.
+      ...oMM0!!*   *!0Mon. +00+.
+ ..o0M0o*               '"* '+0n+
+                             '+no+            .
+               .+oo0M0o+..   . +On+.           'o.
+        .-o0M0o*"       "*oiM00n'+0n+.            *0.
+   .+o00*"                       .00io+. ....        *o.%s
+                                +o0000iiiioooooon+.    %s'0o.%s
+                             +oMMMMMMMM000000000MMMMOOn.%s '0!.%s
+                           o00000+*'              '+00MMo....
+                         oMMM0*                      '+0MMM00Mi
+                       oMMM0*                        .ii000'%s0o
+                      oMM0*                           '+ii0000o,
+                     oMM0'                               '+i0MM0o,
+                    o0M0'                                  '+iMiio
+                    oMM0                                     'ii
+                    oMM0
+                    o0M0.
+                    'oOMn.
+                     'o0MMn.
+                      'oOOM0n+..
+                        '*MMMMMMoo+...
+                           "**######MMMMMMMMMMMMMMMMMMMMM000oo+.
+                                "***iii00000000000000000  '"*O00o+
+                                                     '+00+     "*0i+
+                                                        +ii.      *Mo.
+                                                          'ii       *Mo.
+                                                           'ii.       *io.
+                                                             io.        0o.
+                                                              '!.        "0.
+                                                               '0.        "!
+                                                                 "
+"""%(ef, fe, ef, fe, ef, fe, n)
+
+if __name__ == '__main__':
+    xcode_banner()
